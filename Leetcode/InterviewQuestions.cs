476767f1fc839c0dc19561606fac55e2597c6952
avi <avi.cherny@gmail.com>
2022-01-09 20:50:17 +0200

    liveperson interview

---
+++ Leetcode/InterviewQuestions.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    class InterviewQuestions
+    {
+        /// <summary>
+        /// LivePerson
+        /// Q: create generic function that elevator moves to the requested floors based on minimum distance algorithm 
+        ///Example: reqFloors = 2,1,9,8,7,3  / 1,5,8
+        ///current floor = 6 > 7 > 8 > 9 > 3 > 2 > 1 / > 5 > 8 > 1
+        ///Current floor = 4 > 3 > 2 > 1 > 7 > 8 > 9
+        ///Current floor = 5 randomly, same logic
+        //algorithm = minimum closest distance
+
+        /// </summary>
+        /// <param name="reqFloors"></param>
+        /// <param name="currentFloor"></param>
+        public static void GoToNextFloor(List<int> reqFloors, int currentFloor)
+        {
+            var copyCollection = reqFloors;
+            var allDistances = new Dictionary<int, int>();
+            var floorsAlreadyViseted = new List<int>();
+            var minDistance = int.MaxValue;
+            var allMinDistances = new List<int>();
+
+            for (int i = 0; i < copyCollection.Count; i++)
+            {
+                foreach (var floor in copyCollection)
+                {
+                    if (floorsAlreadyViseted.Contains(floor))
+                    {
+                        continue;
+                    }
+
+                    int distanceFromCurrentFloors = (Math.Abs(currentFloor - floor));
+                    allDistances.Add(floor, distanceFromCurrentFloors);
+                    minDistance = Math.Min(distanceFromCurrentFloors, minDistance);
+                }
+
+                int nextFloor = 0;
+                foreach (var value in allDistances)
+                {
+                    if (value.Value == minDistance)
+                    {
+                        allMinDistances.Add(value.Key);
+                        nextFloor = value.Key;
+                    }
+                }
+
+                var random = new Random();
+                if (allMinDistances.Count > 1)
+                {
+                    int randomSelectedFloor = random.Next(allMinDistances.Count);
+                    currentFloor = allMinDistances[randomSelectedFloor];
+                }
+
+                else
+                {
+                    currentFloor = nextFloor;
+                }
+
+                floorsAlreadyViseted.Add(currentFloor);
+                allDistances.Clear();
+                allMinDistances.Clear();
+                minDistance = int.MaxValue;
+            }
+
+            Console.WriteLine($"visited floors: ");
+            foreach (var floor in floorsAlreadyViseted)
+            {
+                Console.Write($"{floor} -> ");
+            }
+        }
+    }
+}
---
+++ Leetcode/StackSolutions.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    class StackSolutions
+    {
+        public static bool IsValidParentheses(string s)
+        {
+            Stack<char> parentheses = new Stack<char>();
+            {
+                foreach (var sign in s)
+                {
+                    if (sign == '(')
+                    {
+                        parentheses.Push(')');
+                    }
+                    else if (sign == '[')
+                    {
+                        parentheses.Push(']');
+                    }
+                    else if (sign == '{')
+                    {
+                        parentheses.Push('}');
+                    }
+                    else
+                    {
+                        if (parentheses.Count == 0)
+                            return false;
+                        else if (parentheses.Pop() != sign)
+                        {
+                            return false;
+                        }
+                    }
+                }
+                if (parentheses.Count > 0)
+                    return false;
+                return true;
+            }
+
+        }
+
+        public static void GoToNextFloor(List<int> reqFloors, int currentFloor)
+        {
+            var copyCollection = reqFloors;
+            var allDistances = new Dictionary<int, int>();
+            var floorsAlreadyViseted = new List<int>();
+            var minDistance = int.MaxValue;
+            var allMinDistances = new List<int>();
+
+            for (int i = 0; i < copyCollection.Count; i++)
+            {
+                foreach (var floor in copyCollection)
+                {
+                    if (floorsAlreadyViseted.Contains(floor))
+                    {
+                        continue;
+                    }
+
+                    int distanceFromCurrentFloors = (Math.Abs(currentFloor - floor));
+                    allDistances.Add(floor, distanceFromCurrentFloors);
+                    minDistance = Math.Min(distanceFromCurrentFloors, minDistance);
+                }
+
+                int nextFloor = 0;
+                foreach (var value in allDistances)
+                {
+                    if (value.Value == minDistance)
+                    {
+                        allMinDistances.Add(value.Key);
+                        nextFloor = value.Key;
+                    }
+                }
+
+                var random = new Random();
+                if (allMinDistances.Count > 1)
+                {
+                    int randomSelectedFloor = random.Next(allMinDistances.Count);
+                    currentFloor = allMinDistances[randomSelectedFloor];
+                }
+
+                else
+                {
+                    currentFloor = nextFloor;
+                }
+
+                floorsAlreadyViseted.Add(currentFloor);
+                allDistances.Clear();
+                allMinDistances.Clear();
+                minDistance = int.MaxValue;
+            }
+
+            Console.WriteLine($"visited floors: ");
+            foreach (var floor in floorsAlreadyViseted)
+            {
+                Console.Write($"{floor} -> ");
+            }
+        }
+    }
+}
